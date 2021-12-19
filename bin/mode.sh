1bc393c43225bd53b2e17a73b72bde1047562bef
Andy Elster <andy.elster@gmail.com>
2021-12-18 19:32:50 -0800

    clean up mode to work in both environments

--- bin/mode.sh
+++ bin/mode.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-ans="x"
+if [ "$USER" = "andy" ]; then
+    path="/usr/local/site"
+else
+    path="/home1/joylearn/site"
+fi
+
+mode="x"
 done=0
 while [ $done -eq 0 ]; do
-    case "$ans" in
+    case "$mode" in
         d|p)
             done=1;;
         *)
-            read -p "Enter mode: (D)eveloper or (P)roduction: " -n 1 ans
-            ans=`echo $ans | tr '[:upper:]' '[:lower:]'`
+            read -p "Enter mode: (D)eveloper or (P)roduction: " -n 1 mode
+            mode=`echo $mode | tr '[:upper:]' '[:lower:]'`
             echo;;
     esac
 done
@@ -16,11 +22,11 @@ done
 rm -f -- ../css/Common.css
 rm -f -- ../scripts/Common.js
 
-if [ "$ans" = "p" ]; then
-    ln -v -s /usr/local/site/Common-IHDS-societies/css/Common.css ../css/Common.css
-    ln -v -s /usr/local/site/Common-IHDS-societies/scripts/Common.js ../scripts/Common.js
+if [ "$mode" = "p" ]; then
+    ln -v -s $path/Common-IHDS-Societies/css/Common.css ../css/Common.css
+    ln -v -s $path/Common-IHDS-Societies/scripts/Common.js ../scripts/Common.js
 
 else
-    ln -v -s /usr/local/site/Common/css/Common.css ../css/Common.css
-    ln -v -s /usr/local/site/Common/scripts/Common.js ../scripts/Common.js
+    ln -v -s $path/Common/css/Common.css ../css/Common.css
+    ln -v -s $path/Common/scripts/Common.js ../scripts/Common.js
 fi
