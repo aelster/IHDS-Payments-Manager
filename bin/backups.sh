faf13a503038484035b7bf05dcaca190372cb2a5
Andy Elster <andy.elster@gmail.com>
2022-12-31 15:57:55 -0800

    Fix CRON backups

---
+++ bin/backups.sh
@@ -0,0 +1,29 @@
+#!/bin/bash 
+
+startDir=`pwd`
+
+ts=`date +"%Y-%m-%d_%H-%M-%S"`
+def="/home1/joylearn/site/ihds/.ihds-dump.cnf"
+db="joylearn_ihds_donors"
+sqlFile="${db}_${ts}.sql"
+mailTo="aelster@irvinehebrewday.org"
+subject="IHDS Backup - ${ts}"
+
+cd /home1/joylearn/site/tmp
+
+mysqldump \
+    --defaults-extra-file=$def \
+    --compact \
+    --no-tablespaces \
+    --add-drop-table \
+    --skip-comments \
+    --skip-extended-insert \
+    $db > $sqlFile
+
+tar -cjf ${sqlFile}.bz2 $sqlFile
+
+echo "" | mail -a ${sqlFile}.bz2 -s "${subject}" $mailTo
+
+echo "Mail sent - $subject"
+
+cd $startDir
